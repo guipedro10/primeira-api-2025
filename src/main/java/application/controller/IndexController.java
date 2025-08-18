0e5edb8b6b2d807634f143057b1bc77ecca4a804
Guilherme Pedro <132401438+guipedro10@users.noreply.github.com>
2025-08-18 17:41:41 +0000

    aula do dia 18_8

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -1,10 +1,9 @@
 package application.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class IndexController {
-
-    //private List<String> frutas = Arrays.asList("Maçã", "Uva", "Banana");
+    //private List<String> frutas = Arrays.asList("Maça", "Uva", "Banana");
     private List<String> frutas = new ArrayList<String>();
+
     @GetMapping
     public List<String> home() {
         return frutas;
